d2e4c3564c8ef0f17a5584fef084628fbeb7f293
Backendeng <backendeng0417@gmail.com>
2022-04-08 19:00:53 +0900

    test2

--- Assets/Scripts/breakable_script.cs
+++ Assets/Scripts/breakable_script.cs
@@ -60,12 +60,14 @@ public class breakable_script: Photon.MonoBehaviour {
 					if (Random.Range(0.0f, 1.0f) > 0.5f) {
 						//  photonView.RPC("RPC_Powerup", PhotonTargets.All);
 						float random = Random.Range(0, 3);
-						if (random < 1 )
+						Debug.Log(random);
+						if (random < 1 ) {
 							PhotonNetwork.Instantiate(Path.Combine("Prefabs", "PowerUp"), transform.position, Quaternion.identity, 0);
-						else if (random < 2 && random > 1)
+						} else if (random < 2 && random > 1) {
 							PhotonNetwork.Instantiate(Path.Combine("Prefabs", "PowerUp1"), transform.position, Quaternion.identity, 0);
-						else if (random > 2 && random < 3)
+						} else if (random > 2 && random < 3) {
 							PhotonNetwork.Instantiate(Path.Combine("Prefabs", "PowerUp2"), transform.position, Quaternion.identity, 0);
+						}
 						// Powerups.transform.GetComponent<powerup_script>().Starts();
 					}
 				}
