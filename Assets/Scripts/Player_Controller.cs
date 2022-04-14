707b91a82f5391acfb0ffda6ea403a04de454937
Backendeng <backendeng0417@gmail.com>
2022-04-14 18:35:31 +0900

    test

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -422,7 +422,7 @@ public class Player_Controller: Photon.MonoBehaviour {
 	[PunRPC]
 	private void killIncrease(int viewID){
 		GameObject ghostMonkey = PhotonView.Find(viewID).gameObject;
-		ghostMonkey.transform.GetComponent<Player_Controller>().count_kill++;
+		ghostMonkey.transform.GetComponent<Player_Controller>().count_kill += 1;
 	}
 
 	private void FurtherRespawn() {
